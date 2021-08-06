d1966acaa7fcd06de988621a0e4cd30ea3950586
yilmaz <yilmazapis@gmail.com>
2021-08-06 07:19:46 -0400

    Updates

--- graphs/walls_gates.py
+++ graphs/walls_gates.py
@@ -3,13 +3,36 @@ You are given a m x n 2D grid initialized with these three possible values.
 
 -1 - A wall or an obstacle.
 0 - A gate.
-INF - Infinity means an empty room.
-Fill each empty room with the distance to its nearest gate. If it is impossible to reach a Gate, that room should remain filled with INF
+float('inf - Infinity means an empty room.
+Fill each empty room with the distance to its nearest gate. If it is impossible to reach a Gate, that room should remain filled with float('inf
 '''
+'''
+Deque is preferred over list in the cases where we need quicker append and pop operations from both the ends of container, as deque provides 
+an O(1) time complexity for append and pop operations as compared to list which provides O(n) time complexity.
+'''
+
 from typing import List
 from collections import deque
+directions = [[-1, 0], [0, 1], [1, 0], [0, -1] ]
 class Solution:
-    def bfs(self,rooms:List[List[int]])->None:
+    def depthFirst(self, rooms):
+        ROWS, COLS = len(rooms), len(rooms[0])
+
+        def backtrack(r, c, count):
+            if r < 0 or r == ROWS or c < 0 or c == COLS or rooms[r][c] == -1 or count > rooms[r][c]:
+                return
+            rooms[r][c] = count
+            for dir in directions:
+                backtrack(r + dir[0], c + dir[1], count + 1);
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                # find all the gates
+                if rooms[r][c] == 0:
+                    backtrack(r, c, 0)
+        print(rooms)
+
+    def breadthFirst(self,rooms:List[List[int]])->None:
         ROWS,COLS=len(rooms),len(rooms[0])
         visit=set()
         q=deque()
@@ -42,3 +65,14 @@ class Solution:
                 addRoom(r,c+1)
                 addRoom(r,c-1)
             dist+=1
+        print(rooms)
+test = [
+  [float('inf'), -1, 0, float('inf')],
+  [float('inf'), float('inf'), float('inf'), 0],
+  [float('inf'), -1, float('inf'), -1],
+  [0, -1, float('inf'), float('inf')]
+];
+
+s=Solution()
+s.breadthFirst(test)
+s.depthFirst(test)
