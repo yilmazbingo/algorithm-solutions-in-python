37a2b23772ec2ba6ff7031e8c01d9ea5604451d6
yilmaz <yilmazapis@gmail.com>
2021-09-23 21:53:58 -0400

    Updates

---
+++ backtracking/max_length_concatenated_str_w_unique_char.py
@@ -0,0 +1,42 @@
+'''
+1239. Maximum Length of a Concatenated String with Unique Characters
+You are given an array of strings arr. A string s is formed by the concatenation of a subsequence of arr that has unique characters.
+Return the maximum possible length of s.
+
+A subsequence is an array that can be derived from another array by deleting some or no elements without changing the order of the remaining elements.
+'''
+
+from typing import List
+class Solution:
+    def maxLength(self, arr: List[str]) -> int:
+        charset = set()
+        def overlap(charsett, string):
+            prev = set()
+            for c in string:
+                if c in prev or c in charsett:
+                    return True
+                prev.add(c)
+            return False
+        count = 1
+        def backtrack(i):
+            nonlocal count
+            count += 1
+            #             print(count)
+            if i == len(arr):
+                return len(charset)
+            res = 0
+            #             print(charset)
+            if not overlap(charset, arr[i]):
+                for c in arr[i]:
+                    charset.add(c)
+                res = backtrack(i + 1)
+                for c in arr[i]:
+                    # this shows that last element that added is removed
+                    print(c)
+                    charset.remove(c)
+            return max(res, backtrack(i + 1))
+        return backtrack(0)
+
+
+s = Solution()
+s.maxLength(["un", "iq", "ae"])
