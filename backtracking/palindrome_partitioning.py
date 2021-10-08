657797da5c34634bbcd9ceba6f4f0bb2c5d5cf05
yilmaz <yilmazapis@gmail.com>
2021-10-08 05:10:48 -0400

    Updates

--- backtracking/palindrome_partitioning.py
+++ backtracking/palindrome_partitioning.py
@@ -8,10 +8,12 @@ class Solution:
             if i == len(s):
                 res.append(part.copy())
                 return
+            # this creates width of the decision tree
             for j in range(i, len(s)):
                 # i cannot pass s[i:j] as an argument
                 if self.palindrome(s, i, j):
                     part.append(s[i:j + 1])
+                    # this creates height of the decision tree
                     dfs(j + 1)
                     part.pop()
         dfs(0)
